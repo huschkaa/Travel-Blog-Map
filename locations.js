26f979693467f37dc9b6d7a7219090fd926acacb
Andrew Huschka <huschkaa@gmail.com>
2018-02-26 21:35:49 -0600

    updated image locations JPG

--- locations.js
+++ locations.js
@@ -3,87 +3,87 @@ var locations = [
     {
         title: 'Mostar, Bosnia and Herzegovina',
         location: {lat: 43.344043, lng: 17.806820},
-        image: 'images/mostar.jpg'
+        image: 'images/mostar.JPG'
     },
     {
         title: 'Dubrovnik, Croatia',
         location: {lat: 42.650659, lng: 18.094416},
-        image: 'images/dubrovnik.jpg'
+        image: 'images/dubrovnik.JPG'
     },
     {
         title: 'Korcula, Croatia',
         location: {lat: 42.930622, lng: 16.888665},
-        image: 'images/korcula.jpg'
+        image: 'images/korcula.JPG'
     },
     {
         title: 'Bol, Croatia',
         location: {lat: 43.261234, lng: 16.654907},
-        image: 'images/bol.jpg'
+        image: 'images/bol.JPG'
     },
     {
         title: 'Budapest, Hungary',
         location: {lat: 47.498759, lng: 19.039999},
-        image: 'images/budapest.jpg'
+        image: 'images/budapest.JPG'
     },
     {
         title: 'Hvar, Croatia',
         location: {lat: 43.172973, lng: 16.441005},
-        image: 'images/hvar.jpg'
+        image: 'images/hvar.JPG'
     },
     {
         title: 'Kravice Falls, Bosnia and Herzegovina',
         location: {lat: 43.156281, lng: 17.607987},
-        image: 'images/kravice_falls.jpg'
+        image: 'images/kravice_falls.JPG'
     },
     {
         title: 'Prague, Czech Republic',
         location: {lat: 50.075865, lng: 14.437805},
-        image: 'images/prague.jpg'
+        image: 'images/prague.JPG'
     },
     {
         title: 'Split, Croatia',
         location: {lat: 43.509256, lng: 16.439813},
-        image: 'images/split.jpg'
+        image: 'images/split.JPG'
     },
     {
         title: 'Cesky Krumlov, Czech Republic',
         location: {lat: 48.813619, lng: 14.317463},
-        image: 'images/cesky_krumlov.jpg'
+        image: 'images/cesky_krumlov.JPG'
     },
     {
         title: 'El Calafate, Argentina',
         location: {lat: -50.337490, lng: -72.265550},
-        image: 'images/calafate.jpg'
+        image: 'images/calafate.JPG'
     },
     {
         title: 'El Chalten, Argentina',
         location: {lat: -49.331397, lng: -72.886313},
-        image: 'images/chalten.jpg'
+        image: 'images/chalten.JPG'
     },
     {
         title: 'Torres Del Paine, Chile',
         location: {lat: -50.930207, lng: -73.428761},
-        image: 'images/torres.jpg'
+        image: 'images/torres.JPG'
     },
     {
         title: 'Punta Arenas, Chile',
         location: {lat: -53.163159, lng: -70.916569},
-        image: 'images/arenas.jpg'
+        image: 'images/arenas.JPG'
     },
     {
         title: 'Santiago, Chile',
         location: {lat: -50.930207, lng: -73.428761},
-        image: 'images/santiago.jpg'
+        image: 'images/santiago.JPG'
     },
     {
         title: 'Colchuaga Valley, Chile',
         location: {lat: -34.684878, lng: -71.091838},
-        image: 'images/colchuaga.jpg'
+        image: 'images/colchuaga.JPG'
     },
     {
         title: 'Macchu Picchu, Peru',
         location: {lat: -13.162932, lng: -72.544984},
-        image: 'images/macchu.jpg'
+        image: 'images/macchu.JPG'
     },
 
 
